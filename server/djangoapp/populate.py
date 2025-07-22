45d3636236768508ff1656dfdf7d9cbefba79fc0
Supreetha <iris.0287@gmail.com>
2025-07-21 23:47:44 -0400

    Lint errors resolved

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -15,7 +15,7 @@ def initiate():
             car_make_instances.append(
                                         CarMake.objects.create(name=data['name'],
                                         description=data['description']
-                                    ))
+                                     ))
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {
