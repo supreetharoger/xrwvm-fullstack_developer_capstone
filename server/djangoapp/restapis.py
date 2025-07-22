94619cacd042a1aed8858779c4bba696363f1296
Supreetha <iris.0287@gmail.com>
2025-07-22 00:13:12 -0400

    Lint errors resolved

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,13 +11,14 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params = params + key + "=" +value + "&" 
+            params = params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
     print("Request_url", backend_url)
