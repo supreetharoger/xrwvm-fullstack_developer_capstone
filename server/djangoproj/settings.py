b3668c2d4ffb4cd14a4267976cdc5f419e3f9c78
Supreetha <iris.0287@gmail.com>
2025-07-22 00:46:25 -0400

    Lint errors resolved

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.'+
+        'django.contrib.auth.' +
         'password_validation.UserAttributeSimilarityValidator',
     },
     {
